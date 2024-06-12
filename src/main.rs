60307ac685d8cdd251bc6b4955ebce1b8d1347d4
Oleg Golibroda <oleg.golibroda@iway.ch>
2024-06-12 08:36:27 +0000

    eh

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,33 @@
 use actix_web::{web, App, HttpServer};
 use std::sync::Mutex;
+use std::fs;
+use std::io;
 
-struct AppStateWithCounter {
-    counter: Mutex<i32>, // <- Mutex is necessary to mutate safely across threads
+struct AppState {
+    path: Mutex<String>,
+    current_file: Mutex<String>,
 }
 
-async fn index(data: web::Data<AppStateWithCounter>) -> String {
-    let mut counter = data.counter.lock().unwrap(); // <- get counter's MutexGuard
-    *counter += 1; // <- access counter inside MutexGuard
+async fn index(data: web::Data<AppState>) -> String {
+    let path = data.path.lock().unwrap();
+    // let paths = fs::read_dir(path.to_string()).unwrap();
+    // for pp in paths {
+    //     println!("Name: {}", pp.unwrap().path().display())
+    // }
 
-    format!("Request number: {counter}") // <- response with count
+    format!("Request number: @ {path}") // <- response with count
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let counter = web::Data::new(AppStateWithCounter {
-        counter: Mutex::new(0),
+    println!("Awaiting Path (Empty string indicates current directory):");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("error: unable to read user input");
+    println!("Looking at: {}",input);
+
+    let counter = web::Data::new(AppState {
+        current_file: Mutex::new("".to_string()),
+        path: Mutex::new(input),
     });
 
     HttpServer::new(move || {
